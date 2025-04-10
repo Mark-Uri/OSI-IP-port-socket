22774eb3ba5ddf6de76f2aefd05aee6449251c39
Марк Юрьевич <krmark46@gmail.com>
2025-04-10 19:07:23 +0300

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Diagnostics;
 using System.Text;
 
 class Client
@@ -7,46 +8,66 @@ class Client
     private const int DEFAULT_BUFLEN = 512;
     private const string DEFAULT_PORT = "27015";
 
+
+
     static void Main()
     {
         Console.Title = "CLIENT SIDE";
+        Console.OutputEncoding = Encoding.UTF8;
+        var ipAddress = IPAddress.Loopback;
+        var remoteEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
+        var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
         try
         {
-            var ipAddress = IPAddress.Loopback; // IP-адрес локального хоста (127.0.0.1), который используется для подключения к серверу на текущем устройстве
-            var remoteEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
-
-            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            clientSocket.Connect(remoteEndPoint); // инициирует подключение клиента к серверу по указанному конечному пункту (IP-адрес и порт)
+            clientSocket.Connect(remoteEndPoint);
             Console.WriteLine("Подключение к серверу установлено.");
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Сервер не найден. Запускаем сервер...");
 
-            while (true)
+            try
             {
-                Console.Write("Напишите запрос к серверу (или наберите exit для выхода): ");
-                var message = Console.ReadLine();
-                if (message == "exit")
-                {
-                    clientSocket.Shutdown(SocketShutdown.Send);
-                    clientSocket.Close();
-                    Console.WriteLine("Соединение с сервером закрыто.");
-                    break;
-                }
-                else
-                {
-                    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                    clientSocket.Send(messageBytes);
-                    Console.WriteLine($"Сообщение отправлено: {message}");
+                
+                Process.Start("C:\\Users\\USER\\Desktop\\Новая папка (2)\\Server\\bin\\Debug\\net9.0\\Server.exe");
+                Thread.Sleep(2000); 
 
-                    var buffer = new byte[DEFAULT_BUFLEN];
-                    int bytesReceived = clientSocket.Receive(buffer);
-                    string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
-                    Console.WriteLine($"Ответ от сервера: {response}");
-                }
+                clientSocket.Connect(remoteEndPoint);
+                Console.WriteLine("Повторное подключение к серверу установлено");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при запуске сервера: {ex.Message}");
+                return;
             }
         }
-        catch (Exception ex)
+
+        while (true)
         {
-            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            Thread.Sleep(2000);
+            Console.Write("Напишите запрос к серверу (или наберите exit для выхода): ");
+            var message = Console.ReadLine();
+            if (message == "exit")
+            {
+
+                clientSocket.Shutdown(SocketShutdown.Send);
+                clientSocket.Close();
+                Console.WriteLine("Соединение с сервером закрыто");
+                break;
+            }
+            else
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(messageBytes);
+
+
+                var buffer = new byte[DEFAULT_BUFLEN];
+                int bytesReceived = clientSocket.Receive(buffer);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            }
         }
+
+
     }
 }
